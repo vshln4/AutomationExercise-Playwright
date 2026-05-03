fb0f87e590d29cd6680e27ac3a61b8e27ebb4aff
Vishal Nagdev <vshln4@gmail.com>
2026-05-03 14:00:32 +0530

    add products test cases

--- pages/products.js
+++ pages/products.js
@@ -1,12 +1,19 @@
 export class products{
     constructor(page){
         this.page = page
-        this.productsUrl =page.getByRole('link',{name:'Products'})
+    
+        this.prodsearch =page.getByPlaceholder('Search Product')
+        this.searchButton =page.locator('#submit_search' )
     }
-    async goto(){
-        await this.page.goto('https://automationexercise.com/login')
-    }
-    async prods(){
-        await this.productsUrl.click()
-    }
+    async goto() {
+    await this.page.goto('https://automationexercise.com/products') 
+  }
+async searchProduct(productName) {
+    await this.prodsearch.fill(productName)
+    await this.searchButton.click({ force: true })
+  }
 }
+ 
+        
+    
+    
--- tests/products.spec.js
+++ tests/products.spec.js
@@ -2,7 +2,7 @@ import {test,expect} from '@playwright/test'
 import { loginAs } from '../helper/helper'
 import { products } from '../pages/products'
 
-test.describe('Products',async () => {
+test.describe('Products', () => {
 let productspage
 
 test.beforeEach(async ({page}) => {
@@ -11,7 +11,13 @@ test.beforeEach(async ({page}) => {
    await productspage.goto()
 })
 test('TC_01 Product page', async({page}) => {
-    await productspage.prods()
-    await expect(page).toHaveURL('https://automationexercise.com/products')
+    
+    await expect(page.getByText('All Products')).toBeVisible()
+
+})
+test('TC02 Search', async({page})=> {
+    
+    await productspage.searchProduct('Top')
+    await expect(page.getByText('Searched Products')).toBeVisible()
 })
 })
